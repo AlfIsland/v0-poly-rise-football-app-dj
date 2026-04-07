9c7a7bf8905902691cbf82ecf50426bbf10e6944
PolyRISE Football <coachjonathan@polyrisefootball.com>
2026-04-07 00:05:15 -0500

    Remove debug endpoint

--- app/api/admin/debug/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  return NextResponse.json({
-    ADMIN_PASSWORD_set: !!process.env.ADMIN_PASSWORD,
-    ADMIN_SESSION_TOKEN_set: !!process.env.ADMIN_SESSION_TOKEN,
-    REDIS_URL_set: !!process.env.REDIS_URL,
-  })
-}
